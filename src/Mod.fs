2d88510a56efcd674d5e1811cb33300298b90be9
alattalatta <urty5656@gmail.com>
2020-05-01 17:00:53 +0900

    🔧 Do not add infusions to single-use weapons

--- src/Mod.fs
+++ src/Mod.fs
@@ -7,10 +7,16 @@ open HarmonyLib
 open RimWorld
 open Verse
 
-open DefTool
+open Lib
+open VerseTools
 
 let hasQualityNoInfusion (def: ThingDef) =
     def.HasComp(typedefof<CompQuality>) && not (def.HasComp(typedefof<Comp.Infusion>))
+
+let isSingleUse (def: ThingDef) =
+    Option.ofObj def.thingSetMakerTags
+    |> Option.map (Seq.contains "SingleUseWeapon")
+    |> Option.defaultValue false
 
 type ModBase() =
     inherit HugsLib.ModBase()
@@ -28,22 +34,21 @@ type ModBase() =
     member private this.InjectToThings() =
         let infusionCandidates =
             DefDatabase<ThingDef>.AllDefs
-            |> Seq.filter apparelOrWeapon
-            |> Seq.filter hasQualityNoInfusion
+            |> Seq.filter (apparelOrWeapon <&> hasQualityNoInfusion <&> (isSingleUse >> not))
 
         let iTabType = typedefof<ITab.Infused>
         let iTab = InspectTabManager.GetSharedInstance(iTabType)
 
-        do Seq.iter (fun (def: ThingDef) ->
-            // Needs to be first, label making is order-dependent
-            // Colorful item of colors (90% T) vs. Colorful item (90% T) of colors
+        infusionCandidates
+        |> Seq.iter (fun def ->
+            // Needs to be the first, label making is order-dependent
             do def.comps.Insert(0, CompProperties(typeof<Comp.Infusion>))
             if def.inspectorTabs.NullOrEmpty() then
                 do def.inspectorTabs <- ResizeArray<Type>(1)
                 do def.inspectorTabsResolved <- ResizeArray<InspectTabBase>(1)
 
             do def.inspectorTabs.Add(iTabType)
-            do def.inspectorTabsResolved.Add(iTab)) infusionCandidates
+            do def.inspectorTabsResolved.Add(iTab))
 
     member private this.InjectToStats() =
         do DefDatabase<StatDef>.AllDefs
