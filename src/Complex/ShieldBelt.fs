31150a3aea399d31f666d4b4bef17857109174d9
alattalatta <urty5656@gmail.com>
2020-05-27 01:15:10 +0900

    🔨 ShieldBelt Complex to use stats instead

--- src/Complex/ShieldBelt.fs
+++ src/Complex/ShieldBelt.fs
@@ -7,6 +7,7 @@ open Infusion
 type ShieldBelt() =
     inherit Complex<InfusionDef>()
 
-    override this.Match thing _ = thing.def.thingClass = typeof<ShieldBelt>
+    override this.Match thing _ =
+        (thing.def.statBases.GetStatValueFromList(StatDefOf.EnergyShieldEnergyMax, 0.0f)) > 0.0f
 
     override this.BuildRequirementString() = Some L18N.Complex.shieldBelt
