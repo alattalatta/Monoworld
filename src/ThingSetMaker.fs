35229c347ebdd92f22ad2d3da84f67d91ac88393
alattalatta <urty5656@gmail.com>
2020-06-02 23:39:53 +0900

    🎁 Give infusers as rewards

---
+++ src/ThingSetMaker.fs
@@ -0,0 +1,36 @@
+namespace Infusion
+
+open RimWorld
+open Verse
+
+open Lib
+
+type ThingSetMakerInfuser() =
+    inherit ThingSetMaker()
+
+    member this.GenerateOne() =
+        DefDatabase<TierDef>.AllDefs
+        |> Seq.filter (Settings.Tiers.isEnabled)
+        |> Option.ofSeq
+        |> Option.map GenCollection.RandomElement
+        |> Option.map (fun tier -> ThingMaker.MakeThing tier.infuser)
+
+    override this.Generate(param, outThings) =
+        let count =
+            Option.ofNullable param.countRange
+            |> Option.map (fun range -> range.RandomInRange)
+            |> Option.defaultValue 1
+
+        let infusers =
+            seq {
+                for _ in 1 .. count do
+                    yield this.GenerateOne()
+            }
+            |> Seq.choose id
+
+        do outThings.AddRange infusers
+
+    override this.AllGeneratableThingsDebugSub(_) =
+        DefDatabase<TierDef>.AllDefs
+        |> Seq.filter (Settings.Tiers.isEnabled)
+        |> Seq.map (fun tier -> tier.infuser)
