af711ff9241cebb0418689008c365941e7373d8c
alattalatta <urty5656@gmail.com>
2021-06-15 22:42:18 +0900

    ✨ feat(Poet): Lyric.Thing.placeThingNear

--- src/Poet/Lyric.Thing.fs
+++ src/Poet/Lyric.Thing.fs
@@ -5,6 +5,12 @@ open Verse
 
 let getComp<'a when 'a :> ThingComp and 'a: null> (thing: Thing) = Option.ofObj (thing.TryGetComp<'a>())
 
+let isDestroyed (t: Thing) = t.Destroyed
+
 let onSameMap (a: Thing) (b: Thing) = a.Map = b.Map
 
-let isDestroyed (t: Thing) = t.Destroyed
+let placeThingNear (coord: IntVec3) (map: Map) (thing: Thing) =
+    if GenPlace.TryPlaceThing(thing, coord, map, ThingPlaceMode.Near) then
+        Some thing
+    else
+        None
