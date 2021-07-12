61674d9a51cac8e6a41288ff2089a0ab9bf58044
alattalatta <urty5656@gmail.com>
2021-07-12 21:20:53 +0900

    🚧 feat(Infusion): Apply multiplier before offset

--- src/Infusion/StatMod.fs
+++ src/Infusion/StatMod.fs
@@ -30,47 +30,54 @@ type StatMod =
         let sb = StringBuilder()
 
         if fneq0 this.multiplier then
-            do sb.Append(this.multiplier.ToString("+0.##%;-0.##%"))
-               |> ignore
+            do
+                sb.Append(this.multiplier.ToString("+0.##%;-0.##%"))
+                |> ignore
 
-            if fneq0 this.multiplier && fneq0 this.offset
-            then do sb.Append(", ") |> ignore
+            if fneq0 this.multiplier && fneq0 this.offset then
+                do sb.Append(", ") |> ignore
 
         if fneq0 this.offset then
-            do sb.Append(this.offset.ToString("+0.##;-0.##"))
-               |> ignore
+            do
+                sb.Append(this.offset.ToString("+0.##;-0.##"))
+                |> ignore
 
         string sb
 
     static member empty = StatMod()
 
-    static member (+)(a: StatMod, b: StatMod) = StatMod(a.offset + b.offset, a.multiplier + b.multiplier)
+    static member (+)(a: StatMod, b: StatMod) =
+        StatMod(a.offset + b.offset, a.multiplier + b.multiplier)
 
 let applyTo (value: float32) (statMod: StatMod) =
-    (value + statMod.offset)
-    * (1.0f + statMod.multiplier)
+    value * (1.0f + statMod.multiplier)
+    + statMod.offset
 
 let stringForStat (stat: StatDef) (statMod: StatMod) =
     let sb = StringBuilder()
 
     // multiplier
     if fneq0 statMod.multiplier then
-        do sb.Append(statMod.multiplier.ToString("x+0.##%;x-0.##%"))
-           |> ignore
+        do
+            sb.Append(statMod.multiplier.ToString("x+0.##%;x-0.##%"))
+            |> ignore
 
         // separator
-        if fneq0 statMod.offset then do sb.Append(", ") |> ignore
+        if fneq0 statMod.offset then
+            do sb.Append(", ") |> ignore
 
     // offset
     if fneq0 statMod.offset then
         let styled =
             statMod.offset.ToStringByStyle(stat.ToStringStyleUnfinalized)
-            |> (if isNull stat.formatString
-                then id
-                else (fun str -> System.String.Format(stat.formatString, str)))
+            |> (if isNull stat.formatString then
+                    id
+                else
+                    (fun str -> System.String.Format(stat.formatString, str)))
 
         // force add plus sign, StatDef#formatString don't have it
-        if statMod.offset > 0.0f then do sb.Append("+") |> ignore
+        if statMod.offset > 0.0f then
+            do sb.Append("+") |> ignore
 
         do sb.Append(styled) |> ignore
 
