355fba465caf4a28c64fcf1a4d8dd8e021401456
alattalatta <urty5656@gmail.com>
2021-08-05 22:09:33 +0900

    🔨 fix(Infusion): Do not reinfuse bladelinks

--- src/Infusion/Harmonize/CompBiocodable.fs
+++ src/Infusion/Harmonize/CompBiocodable.fs
@@ -12,20 +12,26 @@ open VerseTools
 [<HarmonyPatch(typeof<CompBiocodable>, "CodeFor")>]
 module CodeFor =
     let Prefix (__instance: CompBiocodable, p: Pawn) =
-        do Thing.getComp<CompInfusion> __instance.parent
-           |> Option.filter (fun _ -> Settings.BiocodeBonus.handle.Value)
-           |> Option.iter (fun comp ->
-               let qualityInt = byte (comp.Quality) + 2uy
+        if (not (__instance :? CompBladelinkWeapon)) then
+            do
+                Thing.getComp<CompInfusion> __instance.parent
+                |> Option.filter (fun _ -> Settings.BiocodeBonus.handle.Value)
+                |> Option.iter
+                    (fun comp ->
+                        let qualityInt = byte (comp.Quality) + 2uy
 
-               // clamp!
-               let quality =
-                   if qualityInt > byte (QualityCategory.Legendary)
-                   then QualityCategory.Legendary
-                   else LanguagePrimitives.EnumOfValue qualityInt
+                        // clamp!
+                        let quality =
+                            if qualityInt > byte (QualityCategory.Legendary) then
+                                QualityCategory.Legendary
+                            else
+                                LanguagePrimitives.EnumOfValue qualityInt
 
-               do comp.Biocoder <- Some __instance
-               do comp.SlotCount <- comp.CalculateSlotCountFor quality
-               do comp.Infusions <- (CompInfusion.pickInfusions quality comp))
+                        do comp.Biocoder <- Some __instance
+                        do comp.SlotCount <- comp.CalculateSlotCountFor quality
+                        do comp.Infusions <- (CompInfusion.pickInfusions quality comp))
+        else
+            Log.Message("Skipping bladelink")
 
     /// Resets a `Thing`'s `HitPoints` to its own `MaxHitPoints` to reflect HP changes caused by infusions.
     let Postfix (__instance: CompQuality) =
