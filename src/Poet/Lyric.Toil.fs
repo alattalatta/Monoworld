8c3acf90e5d143783168287774d8435f2a722aea
alattalatta <urty5656@gmail.com>
2021-08-21 00:11:58 +0900

    🎨 feat(Poet): Replace addFailOn with addEndOn

--- src/Poet/Lyric.Toil.fs
+++ src/Poet/Lyric.Toil.fs
@@ -10,31 +10,22 @@ let setTickAction fn (toil: Toil) =
   do toil.tickAction <- new System.Action(fn)
   toil
 
-let addFailOn fn (toil: Toil) =
-  do toil.FailOn(new System.Func<bool>(fn)) |> ignore
-  toil
-
-let addFailOnCannotTouch targetIndex pathEndMode (toil: Toil) =
-  do
-    toil.FailOnCannotTouch(targetIndex, pathEndMode)
-    |> ignore
-
-  toil
-
-let addFailOnSomeoneInteracting targetIndex (toil: Toil) =
-  do
-    toil.FailOnSomeonePhysicallyInteracting(targetIndex)
-    |> ignore
-
-  toil
-
 let setDefaultCompleteMode mode (toil: Toil) =
   do toil.defaultCompleteMode <- mode
   toil
 
-let addProgressBar targetIndex (toil: Toil) =
-  do
-    toil.WithProgressBarToilDelay(targetIndex)
-    |> ignore
-
+let addEndOn fn (toil: Toil) =
+  toil.AddEndCondition(new System.Func<JobCondition>(fn))
   toil
+
+let addFailOnDestroyedNullOrForbidden targetIndex (toil: Toil) =
+  toil.FailOnDestroyedNullOrForbidden(targetIndex)
+
+let addFailOnCannotTouch targetIndex pathEndMode (toil: Toil) =
+  toil.FailOnCannotTouch(targetIndex, pathEndMode)
+
+let addFailOnSomeoneInteracting targetIndex (toil: Toil) =
+  toil.FailOnSomeonePhysicallyInteracting(targetIndex)
+
+let addProgressBar targetIndex (toil: Toil) =
+  toil.WithProgressBarToilDelay(targetIndex)
