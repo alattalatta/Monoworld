faf6420e95638654d7a7d384bbfb32013592a873
alattalatta <urty5656@gmail.com>
2022-11-03 17:20:36 +0900

    feat(Poet): Add err/warn

--- Poet/Lyric.Console.fs
+++ Poet/Lyric.Console.fs
@@ -3,4 +3,6 @@ module Poet.Lyric.Console
 open Verse
 
 
+let err format = Printf.ksprintf Log.Error format
 let log format = Printf.ksprintf Log.Message format
+let warn format = Printf.ksprintf Log.Warning format
